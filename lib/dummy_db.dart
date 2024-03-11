cb71a49fbf135a0a2f0ef62a665e256cdc452333
Shebin-shaji <shebinshaji2615@gmail.com>
2024-03-11 11:08:23 +0530

    search screen completed

--- lib/dummy_db.dart
+++ lib/dummy_db.dart
@@ -5,11 +5,55 @@ class DummyDb {
     "https://collider.com/wp-content/uploads/2019/04/thriller-netflix-movie-poster.png"
         "https://www.digitaltrends.com/wp-content/uploads/2023/11/blQaj6biyBMLo34cuFKKwbgjIBz.jpg?p=1#038;p=1.jpg"
         "https://www.digitaltrends.com/wp-content/uploads/2022/04/6ms0qel7uk2gdffbhfnwuylsq4h.jpg"
-        "https://dnm.nflximg.net/api/v6/2DuQlx0fM4wd1nzqm5BFBi6ILa8/AAAAQVq22SLv3P2Y08ugPkQZCVIrFGuzr8tuSZ3KmNmsMs26VcWgOcxn0a30LUcWGIBOO1dsXZ5dBirPp5uJcIPAO38KqoMYAnkcF3jv5op297DsDnYPTlldkVpXRoy5uyE2z2irr1lOQq7M0H24g_IVVWAD.jpg?r=50a"
         "https://m.media-amazon.com/images/M/MV5BZWU3NjUyODMtNGFlNS00MDg5LWJkNDYtNzc3MzFmZWI5ZjMyXkEyXkFqcGdeQXVyMjkxNzQ1NDI@._V1_FMjpg_UX1000_.jpg"
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHz1ErCl8NJYrUr6vrW6HmBW3Sqv59qCAaCg&usqp=CAU"
         "https://media1.popsugar-assets.com/files/thumbor/d0PiFMVXeVjdBZGtVAvDwOICiXY=/fit-in/792x990/filters:format_auto():upscale()/2023/04/28/965/n/1922283/1b097988a2ba76e7_MCDWEAT_ZX001.jpg"
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjZbnEHdOUoAo_XV34feEVs27LUISOUTg4SlbB-Ff4ej8s7XvZypfQzemuinXDPHkRIQs&usqp=CAU"
-        
+  ];
+
+  static const List<Map> searchList = [
+    {
+      "filmName": "The Impossible",
+      "filmBackground":
+          "https://livijumpa2.files.wordpress.com/2013/12/impossible.jpg"
+    },
+    {
+      "filmName": "1917",
+      "filmBackground":
+          "https://i.ebayimg.com/images/g/GtEAAOSw1W9eN1cY/s-l1600.jpg"
+    },
+    {
+      "filmName": "Oblivion",
+      "filmBackground":
+          "https://images.squarespace-cdn.com/content/v1/507b2f30e4b066e116488db6/1366581664395-1EOF0V8KL1COY6OENC67/Oblivion-Poster.jpg"
+    },
+    {
+      "filmName": "LAND of Mine",
+      "filmBackground":
+          "https://www.coffeeandcigarettes.co.uk/wp-content/uploads/2017/08/LandOfMine_CCWebsite_1600x900-1.jpg"
+    },
+    {
+      "filmName": "THE RAILWAYMAN",
+      "filmBackground":
+          "https://www.telegraph.co.uk/content/dam/films/2017/06/29/14061801551117207912327206_trans_NvBQzQNjv4BqqVzuuqpFlyLIwiB6NTmJwfSVWeZ_vEN7c6bHu2jJnT8.jpg?imwidth=480"
+    },
+    {
+      "filmName": "LA LA LAND",
+      "filmBackground":
+          "https://www.vintagemovieposters.co.uk/wp-content/uploads/2017/11/IMG_6059.jpg"
+    },
+    {
+      "filmName": "OPPENHEIMER",
+      "filmBackground":
+          "https://assets-global.website-files.com/630d4d1c4a462569dd189855/64c19105282a4f4d3e2ba884_2c130933-15f5-4898-a6d6-3d5180b7c2e8%201.webp"
+    },
+    {
+      "filmName": "The Reverend",
+      "filmBackground":
+          "https://dcassetcdn.com/design_img/16298/35582/35582_406182_16298_image.jpg"
+    },
+    {
+      "filmName": "JOKER",
+      "filmBackground":
+          "https://cdna.artstation.com/p/assets/images/images/017/022/542/large/amirhosein-naseri-desktop-screenshot-2019-04-03-18-17-47-11.jpg?1554338571"
+    },
   ];
 }
